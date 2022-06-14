7e12f8c91b394d0d5c489cf160c0c93871ddbbdf
Matheus Garcia <mgarcia.developer@gmail.com>
2022-06-14 20:44:59 -0300

    Matriz sendo criada

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Books from "./Pages/Books";
 import Movies from "./Pages/Movies";
 import Home from "./Pages/Home";
+import Matriz from "./Components/Matriz/Matriz";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/home" exact element={<Home />} />
-            <Route path="/books" exact element={<Books />} />
-            <Route path="/movies" exact element={<Movies />} />
-          </Routes>
-        </BrowserRouter>
+        <Matriz>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/home" exact element={<Home />} />
+              <Route path="/books" exact element={<Books />} />
+              <Route path="/movies" exact element={<Movies />} />
+            </Routes>
+          </BrowserRouter>
+        </Matriz>
       </header>
     </div>
   );
